a2958fc082432c513e4c3ebb490d3637295a5b07
cmhernandezdel <cmhernandezdel@gmail.com>
2019-06-16 17:13:26 +0200

    Fixed read from file function, added an example of distances matrix as a file

--- main.py
+++ main.py
@@ -6,8 +6,22 @@ def read_distances_table(distances_file):
   """
 
   with open(distances_file, 'r') as distances_matrix:
-    # Get the names of the cities and how many of them there are
+    # Get the names of the cities
     cities = distances_matrix.readline().rstrip().split(' ')[2:]
-    number_of_cities = len(cities)
 
     # Get the distances between the cities and store them in a data structure
+    distances_table = dict()
+    for city_key in cities:
+      distances_table[city_key] = dict()
+      distances_from_this_city = distances_matrix.readline().rstrip().split(' ')[1:]
+      city_n = 0
+      for city in cities:
+        distances_table[city_key][city] = distances_from_this_city[city_n]
+        city_n += 1
+
+    # Return distances table
+    return distances_table
+
+
+distances = read_distances_table("distances.txt")
+print(distances)
